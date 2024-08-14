cc1fd8e0201a167b6283af02a9983b6f6b638ef5
ifeanyiOkey <ifeanyi.okey@gmail.com>
2024-08-14 14:31:29 +0100

    My initial project commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,107 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let result = 1;
     
-    return result;
+    let num = input.match(/^[\d./]+/g);
+    // console.log(num.toString().match(/(\.)/));
+
+    if (num != null) {
+      // check double fraction
+      let chkFrac = num[0].split('/');
+      console.log(chkFrac);
+      if (chkFrac.length > 2) {
+        result = 'invalid number'
+      } else {
+        result = eval(num.toString());
+      }
+      // if (eval(num.toString()) == NaN) throw new Error('invalid number');
+
+      // check NaN - multiple decimal
+      // result = parseFloat(chkFrac[0] || chkFrac[0]/chkFrac[1]);
+      // if (chkFrac.length == 1) result = eval(chkFrac[0])
+      // if (chkFrac.length == 2) result = parseFloat(chkFrac[0]/chkFrac[1])
+
+      // if (result == NaN) console.log('invalid number');
+      
+      // console.log(result);
+    }
+    console.log(result);
+    return result = Math.round((result + Number.EPSILON)*100000)/100000;
   };
   
   this.getUnit = function(input) {
     let result;
+
+    let unitIndex = input.search(/[a-zA-z]/);
+    result = input.slice(unitIndex).toLowerCase();
+
+    if (result == 'gal' || result == 'l' || result == 'mi'
+      || result == 'km' || result == 'lbs' || result == 'kg') {
+        result = result;
+      } else {
+        result = 'invalid unit'
+      }
+    if (result == 'l') result = 'L';
+    
+    // console.log(result);
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+
+    switch (initUnit) {
+      case 'gal':
+        result = 'L';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'km':
+        result = 'mi';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'kg':
+        result = 'lbs';
+        break;
+      default:
+        result = 'invalid unit'
+    }
+    // console.log(result);
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
+    let result = 'invalid unit';
+
+    switch (unit) {
+      case 'gal':
+        result = 'gallons';
+        break;
+      case 'L':
+        result = 'liters';
+        break;
+      case 'mi':
+        result = 'miles';
+        break;
+      case 'km':
+        result = 'kilometers';
+        break;
+      case 'lbs':
+        result = 'pounds';
+        break;
+      case 'kg':
+        result = 'kilogram';
+        break;
+    }
+    // console.log(result);
     return result;
   };
   
@@ -29,12 +110,37 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
-    return result;
+
+    switch (initUnit) {
+      case 'gal':
+        result = initNum * galToL;
+        break;
+      case 'L':
+        result = initNum / galToL;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'kg':
+        result = initNum / lbsToKg;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;
+      case 'km':
+        result = initNum / miToKm;
+        break;
+    }
+    return result = Math.round((result + Number.EPSILON)*100000)/100000;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
+
+    result = 
+    `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
+
+    console.log('Getstring: '+result);
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,33 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let initNum;
+    // catch errors (e.g. double decimal) on number input
+    try {
+      initNum = convertHandler.getNum(input);
+    } catch (err) {
+      console.log(err);
+      res.json('invalid number');
+    }
+    let initUnit = convertHandler.getUnit(input);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let spellOutUnit = convertHandler.spellOutUnit(returnUnit);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    
+
+    if (initNum == 'invalid number' && initUnit == 'invalid unit') 
+      return res.json('invalid number and unit');
+    
+    if (initNum == 'invalid number') return res.json('invalid number');
+    if (initUnit == 'invalid unit') return res.json('invalid unit');
+    
+    let result = { initNum, initUnit, returnNum, returnUnit, string }
+    return res.json(result);
+    
+  })
 
 };
